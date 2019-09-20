71e8bb0be9e4b3c35370a3981ccd92a1cad970b4
zhangjunlin <zhangjunlin@hupu.com>
2019-09-20 18:45:46 +0800

    修改调用API接口getPhoto

--- src/main/java/com/fqyb/god/controller/UIController.java
+++ src/main/java/com/fqyb/god/controller/UIController.java
@@ -1,14 +1,19 @@
 package com.fqyb.god.controller;
 
+import com.fasterxml.jackson.databind.*;
+import com.fqyb.god.pojo.ApiCallback;
 import com.fqyb.god.util.AipUtil;
 import com.fqyb.god.util.ImgUtil;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import sun.misc.BASE64Decoder;
 
+
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author: zhaoxudong
@@ -18,21 +23,38 @@ import java.util.HashMap;
 @Controller
 @RequestMapping
 public class UIController {
-    @ResponseBody
-    @PostMapping("/")
-    private String test(@RequestBody HashMap<String,Object> map) throws IOException {
-        HashMap<String, String> options = new HashMap<String, String>();
-        byte[] bytes = new BASE64Decoder().decodeBuffer(ImgUtil.replacePre(map.get("base64").toString()));
-        return AipUtil.getClient().gesture(bytes,options).toString();
-    }
 
-    @GetMapping("test2")
-    private String test2(){
+    @GetMapping("/takePhoto")
+    private String takePhoto(){
         return "takePhoto";
     }
     @ResponseBody
     @PostMapping("/getPhoto")
-    private String test3(@RequestBody HashMap<String,Object> map){
-        return map.get("base64").toString();
+    private String getPhoto(@RequestBody HashMap<String,Object> map) throws IOException{
+        HashMap<String, String> options = new HashMap<>();
+        //解析base64
+        byte[] bytes = new BASE64Decoder().decodeBuffer(ImgUtil.replacePre(map.get("base64").toString()));
+        //通过API获取图片解析结果
+        JSONObject rst = AipUtil.getClient().gesture(bytes, options);
+
+        int result_num = rst.getInt("result_num");
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        if (result_num >0) {
+            JSONArray result = rst.getJSONArray("result");
+            for (int i= 0;i<result.length();i++){
+                JSONObject jsonRst = result.getJSONObject(i);
+                System.out.println(jsonRst);
+                if (!jsonRst.get("classname").equals("Face")){
+                    return jsonRst.toString();
+                }
+
+            }
+
+        }
+
+        return null;
     }
 }
---
+++ src/main/java/com/fqyb/god/pojo/ApiCallback.java
@@ -0,0 +1,58 @@
+package com.fqyb.god.pojo;
+
+public class ApiCallback {
+   private int probability;
+   private int top;
+   private int height;
+   private String classname;
+   private int width;
+   private int left;
+
+    public int getProbability() {
+        return probability;
+    }
+
+    public void setProbability(int probability) {
+        this.probability = probability;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLeft() {
+        return left;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+}
